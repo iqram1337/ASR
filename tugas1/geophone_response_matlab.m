a68c8148869bd12c0faea8b5c5457e3958ef3310
Iqram Haris Fahromi <iqramharisfahromi@yahoo.com>
2023-02-21 12:23:02 +0700

    Add files via upload

---
+++ tugas1/geophone_response_matlab.m
@@ -0,0 +1,23 @@
+clear
+clc
+
+d = 20;
+%N = 6;
+k = linspace(0,0.2,10000);
+
+for i = 1:12
+    ihf(d,i)
+    hold on;
+    axis([00 0.2 -45 20]);
+    grid on;
+end
+
+function ihf(d, N)
+    k = linspace(0,0.2,10000);
+    a = N*sin(pi*d*N*k);
+    b = N*sin(pi*d*k);
+    
+    dB=20*log10(a./b);
+
+    plot(k,dB)
+end
